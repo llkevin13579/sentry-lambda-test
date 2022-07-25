1ec30a445dc66504c3121308b9f9b87290764f46
Kevin Liang <lianglikun@deepbaysz.com>
2022-07-25 23:01:50 +0800

    sentry integrate

--- handler.js
+++ handler.js
@@ -1,21 +1,13 @@
-const Sentry = require("@sentry/serverless");
+const withSentry = require("serverless-sentry-lib");
 
-Sentry.AWSLambda.init({
-  dsn: "https://639b4db1818e40b788b37996ed0c9320@o446118.ingest.sentry.io/6593773",
-  
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
-
-module.exports.hello = Sentry.AWSLambda.wrapHandler(async (event) => {
+module.exports.hello = withSentry(async (event) => {
   const a = 4 /0;
+  menubar();
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0 Lambda! Your function executed successfully!",
+        message: "2Go Serverless v3.0 Lambda! Your function executed successfully!",
         input: event,
       },
       null,
